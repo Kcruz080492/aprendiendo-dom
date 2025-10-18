d89db04351b5f668bebe8babf325e53c04fb818d
Karla Cruz <kcruz080492@gmail.com>
2025-10-18 10:53:22 -0600

    Add files via upload

---
+++ js/03-scripts.js
@@ -0,0 +1,12 @@
+
+const formulario = document.getElementById('formulario');
+console.log(formulario);
+
+// Si no exist eestará vacio
+
+const noExiste = document.getElementById('no-existe');
+console.log(noExiste);
+
+// Añadir un ID 2 veces, seleccionara el primero que encuentre...
+
+// Existen otro par de selectores que hoy en día son los más comunes, tienen una sintaxis muy similar a la de CSS.
